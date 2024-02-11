e7bffbcf207f3f6d2c5a9372f14f48465740c0e7
Laurenz <git@laure.nz>
2024-02-11 14:05:33 +0100

    add `noqa: gha-pinning` feature

--- check_gha_pinning/__init__.py
+++ check_gha_pinning/__init__.py
@@ -1,8 +1,9 @@
 import pathlib
 import re
 import sys
+import ruamel.yaml
 
-import yaml
+IGNORE_PRAGMA = "noqa: gha-pinning"
 
 _SHA256 = re.compile(r"\b[a-fA-F0-9]{64}\b")
 _SHA1 = re.compile(r"\b[a-f0-9]{40}\b")
@@ -21,16 +22,23 @@ def _check(line: str) -> str | None:
 
 def check_pinning(file: pathlib.Path) -> list[str]:
     try:
-        workflow = yaml.safe_load(file.open()) or {}
-    except yaml.YAMLError as ex:
-        return [f"Error parsing workflow yaml: {ex}"]
+        workflow = ruamel.yaml.YAML().load(file)
+    except ruamel.yaml.YAMLError as ex:
+        return [f"Error parsing yaml: {ex}"]
+    try:
+        jobs = workflow.get("jobs", {})
+    except AttributeError:
+        return []  # yaml is not a mapping on top level
+
     uses = []
-    for _, job in workflow.get("jobs", {}).items():
+    for _, job in jobs.items():
         if u := job.get("uses"):
-            uses.append(u)
+            if IGNORE_PRAGMA not in job.ca:
+                uses.append(u)
         for step in job.get("steps", []):
             if u := step.get("uses"):
-                uses.append(u)
+                if IGNORE_PRAGMA not in step.ca:
+                    uses.append(u)
     return [problem for u in uses if (problem := _check(u))]
 
 
@@ -58,3 +66,7 @@ def main() -> int:
             print()
 
     return 1 if fail else 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
